e983cf25b1b64f83486b3fa11afc057ae7f4868f
http-request <svyatoslav.strygin@gmail.com>
2021-02-16 22:44:24 +0300

    Update startup message.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import app from './app';
 
 app.listen(app.get('port'), () => {
   console.log(
-    '  App is running at http://localhost:%d in %s mode',
+    '\tApp is running at http://localhost:%d in %s mode\n',
     app.get('port'),
     app.get('env')
   );
-  console.log('  Press CTRL-C to stop\n');
+  console.log('\tPress CTRL-C to stop\n');
 });
